b36cf689903563044b15f0acd1f9ca27e0da82ea
dev_ys <102666551+yurnsoolora@users.noreply.github.com>
2023-06-08 20:50:33 +0900

    [unrated] Title: 문자열 정수의 합, Time: 0.07 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String num_str) {
+        int answer = 0;
+        char[] ch = num_str.toCharArray();
+        
+        for (int i=0; i<ch.length; i++) {
+            answer += Character.getNumericValue(ch[i]);
+        }
+        return answer;
+    }
+}
