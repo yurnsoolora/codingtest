129afbc939fe8bbd295096b23451763bad458f32
Jung Yurn Soo <lorajung@naver.com>
2023-06-15 20:45:54 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int even = 0; 
+        int odd = 0;
+        
+        for (int i=0; i<num_list.length; i+=2) {
+            odd += num_list[i];
+        }
+        for (int i=1; i<num_list.length; i+=2) {
+            even += num_list[i];
+        }
+        return even > odd ? even : odd;
+    }
+}
