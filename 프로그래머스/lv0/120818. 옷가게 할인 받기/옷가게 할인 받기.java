c6984635274c8601a45536f2ff51431551ec738d
Jung Yurn Soo <lorajung@naver.com>
2023-06-20 15:16:24 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,15 +1,10 @@
 class Solution {
     public int solution(int price) {
         int answer = 0;
-        if (price < 100000) {
-            answer = price;
-        } else if (price < 300000) {
-            answer = price - (int) Math.round(price * 0.05);
-        } else if (price < 500000) {
-            answer = price - (int) Math.round(price * 0.1);
-        } else {
-            answer = price - (int) Math.round(price * 0.2);
-        }
+        if(price >= 500000) answer = (int)(price*0.8);
+        else if(price >= 300000) answer = (int)(price*0.9);
+        else if (price >= 100000) answer = (int)(price*0.95);
+        else answer = price;
        
         return answer;
     }
