ee661445ad3a9e4a8c68e11bd0d13936e7892dec
Jung Yurn Soo <lorajung@naver.com>
2023-06-22 18:30:14 +0900

    [level 0] Title: 369게임, Time: 0.17 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String str = Integer.toString(order);
+        String[] arr = str.split("");
+        
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i].equals("3") || arr[i].equals("6") || arr[i].equals("9")) {
+               answer++; 
+            }  
+        }
+        return answer;
+    }
+}
