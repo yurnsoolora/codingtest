1134ea6f3ede6a556e85e13ba7308a210a499575
Jung Yurn Soo <lorajung@naver.com>
2023-07-06 20:45:12 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.02 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String phone_number) {
+        char[] chars = phone_number.toCharArray();
+        
+        for (int i = 0; i < chars.length - 4; i++) {
+            chars[i] = '*';
+        }
+        
+        return String.valueOf(chars);
+    }
+}
