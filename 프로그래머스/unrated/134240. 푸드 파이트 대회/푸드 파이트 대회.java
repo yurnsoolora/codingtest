f2acfa4fd05993e1622377ff3c8d0c81ccbc2e84
Jung Yurn Soo <lorajung@naver.com>
2023-07-10 17:22:43 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 13.11 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        
+        for (int i=1; i<food.length; i++) {
+            for (int j=1; j<=food[i]/2; j++) {
+                answer += i;
+            }
+        }
+        answer += 0;
+        
+        for (int i=answer.length()-2; i>=0; i--) {
+            answer += answer.charAt(i);
+        }
+        return answer;
+    }
+}
