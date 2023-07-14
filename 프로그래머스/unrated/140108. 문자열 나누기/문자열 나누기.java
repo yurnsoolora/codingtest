0c86ea4ecf8a6fb81254569fcb9e5c825d6dc8dd
Jung Yurn Soo <lorajung@naver.com>
2023-07-14 14:17:08 +0900

    [unrated] Title: 문자열 나누기, Time: 0.72 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,17 @@
+class Solution {
+  public int solution(String s) {
+		char first = s.charAt(0);
+		int firstNum = 0;
+		int diff = 0;
+		int result = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (firstNum == diff) {
+				result++;
+				first = s.charAt(i);
+			}
+			if (s.charAt(i) == first) firstNum++;
+			else diff++;
+		}
+		return result;
+    }
+}
