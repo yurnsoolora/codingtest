c7a4f13e0a374e558b7addc86da7fda1ca65927f
Jung Yurn Soo <lorajung@naver.com>
2023-07-27 19:00:35 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 2.52 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,19 +1,25 @@
-import java.util.*;
 class Solution {
+    private static final int INF = 1000001;
+
     public int[] solution(int[] arr, int[][] queries) {
-        int[] arr1 = new int[queries.length];
-        Arrays.fill(arr1, Integer.MAX_VALUE);
-       for(int i = 0; i < queries.length; i++) { // 쿼리의 길이만큼 반복
-            for (int j = queries[i][0]; j <= queries[i][1]; j++){ 
-            // 각 쿼리의 s <= i <= e의 조건을 for문을 이용해 충족
-                if( arr[j] > queries[i][2]) { // i > k의 조건
-                    if(arr1[i] > arr[j]) // 가장 작은 arr[i] 조건
-                        arr1[i] = arr[j];
-                }
+        int[] answer = new int[queries.length];
+        int idx = 0;
+
+        for (int[] query : queries) {
+            int s = query[0];
+            int e = query[1];
+            int k = query[2];
+
+            int result = INF;
+            for (int i = s; i <= e; i++) {
+                if (arr[i] <= k || arr[i] >= result) continue;
+
+                result = arr[i];
             }
-            if(arr1[i] == Integer.MAX_VALUE)
-                arr1[i] = -1;
+
+            answer[idx++] = result == INF ? -1 : result;
         }
-        return arr1;
+
+        return answer;
     }
 }
