73506b5b360a90f307cf40dfd482e1995eb67d65
Jung Yurn Soo <lorajung@naver.com>
2023-08-04 22:38:51 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.50 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,16 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] emergency) {
         int len = emergency.length;
-        int[] answer = new int [len];
+        int[] answer = new int[len];
         
-        for (int i=0; i<len; i++) {
-            for (int j=0; j<len; j++) {
-                if (emergency[i] < emergency[j]) {
-                    answer[i]++;
-                }
-            }
-            answer[i]++;
+        Map<Integer, Integer> map = new HashMap<>();
+        //map선언
+
+        for (int i = 0; i < len; i++) {
+            map.put(emergency[i], i);
+            //key:응급도
+            //value:인덱스
         }
+
+        Arrays.sort(emergency);
+        //오름차순 정렬
+        //3 24 76
+        //0 2 1
+
+        int rank = emergency.length;
+        for (int i=0; i<len; i++) { //emergency배열 거꾸로 순회하면서
+            answer[map.get(emergency[i])] = rank;
+            rank--;
+        }
+
         return answer;
     }
 }
