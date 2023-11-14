da5a49296124f02d99204094078ad7dfa8bb2a6f
Jung Yurn Soo <lorajung@naver.com>
2023-11-14 11:54:08 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -0,0 +1,17 @@
+def solution(dot):
+    answer = 0
+    x=dot[0]
+    y=dot[1]
+    
+    if x>0:
+        if y>0:
+            answer=1
+        else:
+            answer=4
+    else:
+        if y>0:
+            answer=2
+        else:
+            answer=3
+            
+    return answer
