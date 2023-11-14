ec34abbc88b01dc1b7eeff2e52476e2c9256e461
Jung Yurn Soo <lorajung@naver.com>
2023-11-14 11:47:14 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    answer = []
+    even=0
+    odd=0
+    for num in num_list:
+        if num%2==0:
+            even+=1
+        else:
+            odd+=1
+    answer.append(even)
+    answer.append(odd)
+    return answer
