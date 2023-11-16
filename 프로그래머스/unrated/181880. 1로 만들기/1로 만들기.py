76675e73df4cf28d6ab3723fbe512fe31a4f10d7
Jung Yurn Soo <lorajung@naver.com>
2023-11-16 12:43:59 +0900

    [level unrated] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    answer = 0
+    
+    for num in num_list:
+        
+        while num > 1:
+            answer +=1
+            if num%2==0:
+                num = num//2
+            else:
+                num = (num-1)//2
+        
+    return answer
