ff7c0c6fa310731635146f429746d245fc329b30
Jung Yurn Soo <lorajung@naver.com>
2024-01-24 17:25:02 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 from ANIMAL_INS
-where NAME='Lucy' OR  NAME='Ella' OR  NAME='Pickle' OR  NAME='Rogan' OR NAME='Mitty' OR NAME='Sabrina'
+WHERE NAME IN ('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty')
 order by ANIMAL_ID
